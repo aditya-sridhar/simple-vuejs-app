de069a318bb52913282ae4133c8479f81b82e314
Aditya S <s.aditya.1811@gmail.com>
2018-08-05 23:30:34 +0530

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
@@ -27,5 +23,11 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.centeralign {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
 }
 </style>
